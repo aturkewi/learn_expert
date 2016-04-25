7d9e6b2bc165d7456ec7f6e8a71989c64f579582
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-04-25 08:20:28 -0600

    Add template from the learn-co gem

---
+++ lib/learn_expert/cli.rb
@@ -0,0 +1,22 @@
+module LearnExper
+  class CLI < Thor
+    desc '[test] [options]', "Run a lesson's test suite"
+    long_desc <<-LONGDESC
+      `learn [test] [options]` will run your lesson's test suite.
+
+      You can supply the following options when running Jasmine tests:
+
+      --no-color            # Turn off color output
+      \x5 -l, --local       # Don't push results to Learn
+      \x5 -b, --browser     # Run tests in browser
+      \x5 -o, --out FILE    # Specify an output file
+      \x5 -s, --skip        # Don't run dependency checks
+
+      When running an RSpec test suite, all normal RSpec options can be
+      passed in.
+    LONGDESC
+    def test(*opts)
+      system("learn-test #{opts.join(' ')}")
+    end
+  end
+end
